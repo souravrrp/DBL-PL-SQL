a8dfea3e5b440810ad608cd6078bb5bbedbabae8
Sourav Paul <souravpaulcse@gmail.com>
2022-01-20 09:28:20 +0600

    200122

--- INV/DBL Report/DBL Pharma Stock Report V0.5.sql
+++ INV/DBL Report/DBL Pharma Stock Report V0.5.sql
@@ -1,4 +1,4 @@
-/* Formatted on 1/18/2022 10:48:42 AM (QP5 v5.374) */
+/* Formatted on 1/19/2022 6:01:03 PM (QP5 v5.374) */
 SELECT ledger_name,
        legal_entity_name,
        org_id,
@@ -10,9 +10,23 @@ SELECT ledger_name,
        item_code,
        item_name,
        uom,
-       (pr_quantity - po_quantity) pr_qty,
-       (pr_quantity - (po_quantity + grn_quantity))
+       (CASE
+            WHEN pr_quantity < (po_quantity + grn_quantity)
+            THEN
+                0
+            WHEN pr_quantity > (po_quantity + grn_quantity)
+            THEN
+                pr_quantity - (po_quantity + grn_quantity)
+            ELSE
+                pr_quantity
+        END)
            pr_quantity,
+       (CASE
+            WHEN po_quantity < grn_quantity THEN 0
+            WHEN po_quantity > grn_quantity THEN po_quantity - grn_quantity
+            ELSE po_quantity
+        END)
+           po_qty,
        po_quantity,
        (  (grn_quantity - quarantine_qty)
         - ((  onhand_quantity
@@ -40,12 +54,24 @@ SELECT ledger_name,
                      item_name,
                  msi.primary_uom_code
                      uom,
-                 SUM (prla.quantity)
+                 (SELECT SUM (prla.quantity)
+                    FROM po.po_requisition_lines_all  prla,
+                         po.po_requisition_headers_all prha
+                   WHERE     1 = 1
+                         AND msi.inventory_item_id = prla.item_id(+)
+                         AND msi.organization_id =
+                             prla.destination_organization_id(+)
+                         AND prla.requisition_header_id =
+                             prha.requisition_header_id(+)
+                         --AND pla.item_id IS NOT NULL
+                         AND prla.parent_req_line_id IS NULL)
                      pr_quantity,
                  (SELECT (CASE
-                              WHEN    SUM (pll.quantity_received) = 0
-                                   OR SUM (pll.quantity_received) >
-                                      SUM (pll.quantity)
+                              WHEN SUM (pll.quantity_received) = 0
+                              THEN
+                                  SUM (pla.quantity)
+                              WHEN SUM (pll.quantity_received) >
+                                   SUM (pla.quantity)
                               THEN
                                   SUM (pll.quantity_received)
                               ELSE
@@ -68,18 +94,22 @@ SELECT ledger_name,
                          AND pla.po_line_id = pll.po_line_id(+)
                          AND pda.line_location_id = pll.line_location_id(+))
                      po_quantity,
-                 (SELECT SUM (rt.quantity)     grn_quantity
-                    FROM apps.rcv_shipment_lines rsl, apps.rcv_transactions rt
-                   WHERE     1 = 1
-                         AND rsl.shipment_header_id = rt.shipment_header_id
-                         AND rt.organization_id = msi.organization_id
-                         AND rsl.item_id = msi.inventory_item_id
-                         AND rt.transaction_type = 'RECEIVE'
-                         --AND rt.transaction_type = 'DELIVER'
-                         AND rt.shipment_header_id = rsl.shipment_header_id
-                         AND rt.po_header_id = rsl.po_header_id
-                         AND rt.po_line_id = rsl.po_line_id
-                         AND rt.po_line_location_id = rsl.po_line_location_id)
+                 NVL (
+                     (SELECT SUM (rt.quantity)     grn_quantity
+                        FROM apps.rcv_shipment_lines rsl,
+                             apps.rcv_transactions  rt
+                       WHERE     1 = 1
+                             AND rsl.shipment_header_id = rt.shipment_header_id
+                             AND rt.organization_id = msi.organization_id
+                             AND rsl.item_id = msi.inventory_item_id
+                             AND rt.transaction_type = 'RECEIVE'
+                             --AND rt.transaction_type = 'DELIVER'
+                             AND rt.shipment_header_id = rsl.shipment_header_id
+                             AND rt.po_header_id = rsl.po_header_id
+                             AND rt.po_line_id = rsl.po_line_id
+                             AND rt.po_line_location_id =
+                                 rsl.po_line_location_id),
+                     0)
                      grn_quantity,
                  (SELECT SUM (ohqd.primary_transaction_quantity)
                     FROM apps.mtl_onhand_quantities_detail ohqd
@@ -108,8 +138,6 @@ SELECT ledger_name,
                      reserve_quantity
             FROM inv.mtl_system_items_b           msi,
                  apps.mtl_item_categories_v       cat,
-                 po.po_requisition_lines_all      prla,
-                 po.po_requisition_headers_all    prha,
                  apps.hr_operating_units          hou,
                  apps.xxdbl_company_le_mapping_v  ou,
                  apps.org_organization_definitions ood
@@ -119,23 +147,18 @@ SELECT ledger_name,
                  AND ( :p_item_code IS NULL OR (msi.segment1 = :p_item_code))
                  AND (   :p_item_category IS NULL
                       OR (cat.segment1 = :p_item_category))
+                 -----------------------------------------------------------------------
                  --AND (   :p_date IS NULL OR (TRUNC (pha.approved_date) <= TRUNC ( :p_date)))
                  --AND ( :p_org_id IS NULL OR (pha.org_id = :p_org_id))
                  --AND ( :p_po_no IS NULL OR (pha.segment1 = :p_po_no))
                  --AND ( :p_req_no IS NULL OR (prha.segment1 = :p_req_no))
                  --AND ( :p_item_type IS NULL OR (mc.segment2 = :p_item_type))
+                 -----------------------------------------------------------------------
                  AND ood.organization_id = msi.organization_id(+)
                  AND msi.inventory_item_id = cat.inventory_item_id(+)
                  AND msi.organization_id = cat.organization_id(+)
                  AND ood.operating_unit = ou.org_id(+)
                  AND ood.operating_unit = hou.organization_id(+)
-                 -----------------------------------------------------------------------
-                 AND msi.inventory_item_id = prla.item_id(+)
-                 AND msi.organization_id = prla.destination_organization_id(+)
-                 AND prla.requisition_header_id = prha.requisition_header_id(+)
-                 --AND pla.item_id IS NOT NULL
-                 --AND prha.segment1 IN ('21111003551', '21111003553') --21111003551   --21111003553
-                 AND prla.parent_req_line_id IS NULL
         GROUP BY ou.ledger_name,
                  ou.legal_entity_name,
                  ood.operating_unit,
