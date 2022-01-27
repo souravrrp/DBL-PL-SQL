fc54b33c70f55be884e1713734827dd1a667b812
Sourav Paul <souravpaulcse@gmail.com>
2022-01-27 09:56:38 +0600

    270122

--- APEX/104/250122/Activity List.sql
+++ APEX/104/250122/Activity List.sql
@@ -1,4 +1,4 @@
-/* Formatted on 1/25/2022 5:39:23 PM (QP5 v5.374) */
+/* Formatted on 1/26/2022 10:01:35 AM (QP5 v5.374) */
 SELECT 'PO Generated Through Oracle*' AS activity_list FROM DUAL
 UNION ALL
 SELECT 'LC Opening* ' AS activity_list FROM DUAL
@@ -21,30 +21,135 @@ SELECT 'Once IGM and Rotation completed, CnF will Generate Bill of Entry'    AS 
 UNION ALL
 SELECT 'Common fund requisition for overall operation and office maintenance send to commercial and corporate finance department by CnF'    AS activity_list
   FROM DUAL
-  UNION ALL
+UNION ALL
 SELECT 'After Common Landing, the Consignment Will be Delivered From Port To Depot'    AS activity_list
   FROM DUAL
 UNION ALL
 SELECT 'Completion of Physical Examination/ Lab Test/ Fumigation Test/ Decontamination ((Port/ Depot)'    AS activity_list
   FROM DUAL
 UNION ALL
-SELECT 'Completion of Customs Assessment (Port/ Depot)'    AS activity_list
+SELECT 'Completion of Customs Assessment (Port/ Depot)'     AS activity_list
   FROM DUAL
 UNION ALL
 SELECT 'Bill of entry in case of duty amount send to Commercial and Corporate Finance Department by CnF'    AS activity_list
   FROM DUAL
-  UNION ALL
+UNION ALL
 SELECT 'Checking of Bill of Entry by Commercial and Fund Requisition'    AS activity_list
   FROM DUAL
-  UNION ALL
+UNION ALL
 SELECT 'Payment to Freight Forwarding Agent/ Shipping Line and DO Collection'    AS activity_list
   FROM DUAL
 UNION ALL
 SELECT 'Transport Requisition and Actual Delivery from Port/ Depot'    AS activity_list
   FROM DUAL
 UNION ALL
-SELECT 'Goods Received by Factory'    AS activity_list
-  FROM DUAL
+SELECT 'Goods Received by Factory' AS activity_list FROM DUAL
 UNION ALL
 SELECT 'Empty Container Handover to Depot and BG Release'    AS activity_list
+  FROM DUAL;
+
+------------------------------------------------------------------------------------------------------
+
+/* Formatted on 1/26/2022 10:01:35 AM (QP5 v5.374) */
+SELECT '1 - PO Generated Through Oracle*' AS activity_list FROM DUAL
+UNION ALL
+SELECT '2 - LC Opening* ' AS activity_list FROM DUAL
+UNION ALL
+SELECT '3 - On Board Date of the Goods' AS activity_list FROM DUAL
+UNION ALL
+SELECT '4 - Original Documents Received Through Bank' AS activity_list FROM DUAL
+UNION ALL
+SELECT '5 - Full Set of Original Documents along with Other Docs Sent to CnF'    AS activity_list
   FROM DUAL
+UNION ALL
+SELECT '6 - CnF Received the Docs and Check the Accuracy may depends on Custom Concern'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '7 - IGM Creation by Shipping Line/ Agents and Check by CnF'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '8 - Once IGM and Rotation completed, CnF will Generate Bill of Entry'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '9 - Common fund requisition for overall operation and office maintenance send to commercial and corporate finance department by CnF'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '10 - After Common Landing, the Consignment Will be Delivered From Port To Depot'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '11 - Completion of Physical Examination/ Lab Test/ Fumigation Test/ Decontamination ((Port/ Depot)'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '12 - Completion of Customs Assessment (Port/ Depot)'     AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '13 - Bill of entry in case of duty amount send to Commercial and Corporate Finance Department by CnF'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '14 - Checking of Bill of Entry by Commercial and Fund Requisition'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '15 - Payment to Freight Forwarding Agent/ Shipping Line and DO Collection'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '16 - Transport Requisition and Actual Delivery from Port/ Depot'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '17 - Goods Received by Factory' AS activity_list FROM DUAL
+UNION ALL
+SELECT '18 - Empty Container Handover to Depot and BG Release'    AS activity_list
+  FROM DUAL;
+  
+------------------------------------------------------------------------------------------------------
+
+/* Formatted on 1/26/2022 10:01:35 AM (QP5 v5.374) */
+SELECT '1. PO Generated Through Oracle*' AS activity_list FROM DUAL
+UNION ALL
+SELECT '2. LC Opening* ' AS activity_list FROM DUAL
+UNION ALL
+SELECT '3. On Board Date of the Goods' AS activity_list FROM DUAL
+UNION ALL
+SELECT '4. Original Documents Received Through Bank' AS activity_list FROM DUAL
+UNION ALL
+SELECT '5. Full Set of Original Documents along with Other Docs Sent to CnF'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '6. CnF Received the Docs and Check the Accuracy may depends on Custom Concern'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '7. IGM Creation by Shipping Line/ Agents and Check by CnF'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '8. Once IGM and Rotation completed, CnF will Generate Bill of Entry'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '9. Common fund requisition for overall operation and office maintenance send to commercial and corporate finance department by CnF'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '10. After Common Landing, the Consignment Will be Delivered From Port To Depot'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '11. Completion of Physical Examination/ Lab Test/ Fumigation Test/ Decontamination ((Port/ Depot)'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '12. Completion of Customs Assessment (Port/ Depot)'     AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '13. Bill of entry in case of duty amount send to Commercial and Corporate Finance Department by CnF'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '14. Checking of Bill of Entry by Commercial and Fund Requisition'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '15. Payment to Freight Forwarding Agent/ Shipping Line and DO Collection'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '16. Transport Requisition and Actual Delivery from Port/ Depot'    AS activity_list
+  FROM DUAL
+UNION ALL
+SELECT '17. Goods Received by Factory' AS activity_list FROM DUAL
+UNION ALL
+SELECT '18. Empty Container Handover to Depot and BG Release'    AS activity_list
+  FROM DUAL;  
+  
+  
