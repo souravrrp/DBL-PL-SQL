1f469f357ffcddfc595c83c581acf9adcce9be5e
Sourav Paul <souravpaulcse@gmail.com>
2022-01-30 09:34:46 +0600

    300122

---
+++ APEX/110/260122/Checking.sql
@@ -0,0 +1,11 @@
+/* Formatted on 1/27/2022 4:50:59 PM (QP5 v5.374) */
+SELECT lc.lc_number
+  FROM xxdbl.xx_lc_details lc;
+
+SELECT * FROM rcv_shipment_headers;
+
+SELECT *
+  FROM hr_operating_units hou;
+
+SELECT *
+  FROM apps.ap_suppliers aps;
